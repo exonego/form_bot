667977fd0c090a68ff4b7dfe0b596e42e9493cf3
Pavel R <334554.rud@gmail.com>
2025-08-23 21:14:35 +0300

    changes

--- config/config.py
+++ config/config.py
@@ -5,6 +5,7 @@ from environs import Env
 @dataclass
 class BotSettings:
     token: str
+    ban_list: list[int]
 
 
 @dataclass
@@ -25,7 +26,9 @@ def load_config(path: str | None = None) -> Config:
     env.read_env(path)
 
     return Config(
-        BotSettings(token=env("BOT_TOKEN")),
+        BotSettings(
+            token=env("BOT_TOKEN"), ban_list=env.list("BOT_BAN_LIST", subcast=int)
+        ),
         LogSettings(
             level=env("LOG_LEVEL"), format=env("LOG_FORMAT"), style=env("LOG_STYLE")
         ),
