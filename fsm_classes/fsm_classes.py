cbb3ffbfd7b90a0216a4514e26b693effe140ede
Pavel R <334554.rud@gmail.com>
2025-08-24 18:13:26 +0300

    a

---
+++ fsm_classes/fsm_classes.py
@@ -0,0 +1,8 @@
+from aiogram.fsm.state import State, StatesGroup
+
+
+class FSMFillForm(StatesGroup):
+    fill_name = State()
+    fill_age = State()
+    fill_sex = State()
+    send_contact = State()
