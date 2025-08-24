8fc037543837d7db5ec76dcc39c2620a970eda42
exonego <334554.rud@gmail.com>
2025-08-24 20:30:31 +0300

    Delete database directory

--- database/database.py
+++
@@ -1,8 +0,0 @@
-# init database
-def init_db():
-    return {
-        "user_template": {
-            "form": {"name": None, "age": None, "sex": None, "phone_number": None}
-        },
-        "users": dict(),
-    }
