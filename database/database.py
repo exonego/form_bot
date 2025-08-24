0e9fea607e3cfb2ac6fa22f260bbb8c26d51f286
Pavel R <334554.rud@gmail.com>
2025-08-24 14:51:22 +0300

    lexicon!

--- database/database.py
+++ database/database.py
@@ -1,3 +1,8 @@
 # init database
 def init_db():
-    return {"user_template": {"form": dict()}, "users": dict()}
+    return {
+        "user_template": {
+            "form": {"name": None, "age": None, "sex": None, "phone_number": None}
+        },
+        "users": dict(),
+    }
